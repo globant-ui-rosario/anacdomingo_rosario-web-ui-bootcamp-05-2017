67f9ee817c5e0db076ff17555b25745fbcb518d0
AnaCDomingo <anaclaradomingo@gmail.com>
2017-06-15 01:10:50 -0300

    Please tell me if I got it right this time

--- 02-OOP-Inheritance/Ex05/src/classEventEmitter.js
+++ 02-OOP-Inheritance/Ex05/src/classEventEmitter.js
@@ -1,20 +1,30 @@
 class eventEmitter {
-	constructor() {
-		this.events = ['play', 'resume', 'pause'];
+	constructor(events) {
+		this.events (eventName, callbacks) = {
+			this.eventName: this.eventName,
+			this.callbacks: [],
+		};
 	}
 	on(eventName, callback) {
-		if(this.events[eventName] != callback) {
-			callback();
+		if(this.events.eventName === eventName) {
+			let i = this.events.callbacks.length;
+			this.events.callbacks[i+1] = callback;
 		}
 	}
 	emit(eventName) {
-		if (this.events[eventName]) {
-			this.events[eventName];
+		if (this.events.eventName === eventName) {
+			for (var i=0; i<=this.events.callbacks.length; i++) {
+				this.events.callbacks[i];
+			}
 		}
 	}
 	off(eventName, callback) {
-		if(this.events[eventName]) {
-			callback();
+		if(this.events.eventName === eventName) {
+			for (var i=0; i<=this.events.callbacks.length; i++) {
+				if (this.events.callbacks[i] === callback) {
+					this.events.callbacks[i] = null;
+				}
+			}
 		}
 	}
 }
